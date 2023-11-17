357d9322061a36addd005e7f9db912faf437eb6a
saancheez009 <114072102+saancheez009@users.noreply.github.com>
2023-11-17 12:47:44 +0100

    .

--- ListaContactosFB/listaContactosFBase/app/src/main/java/com/example/ejemplorv/Contacto.kt
+++ ListaContactosFB/listaContactosFBase/app/src/main/java/com/example/ejemplorv/Contacto.kt
@@ -2,6 +2,6 @@ package com.example.ejemplorv
 
 import android.media.Image
 
-data class Contacto (val nombre:String, val telefono:String,val gender:String){
+data class Contacto (   val id: Int , val nombre:String, val telefono:String,val gender:String){
 
 }
--- ListaContactosFB/listaContactosFBase/app/src/main/java/com/example/ejemplorv/MainActivity.kt
+++ ListaContactosFB/listaContactosFBase/app/src/main/java/com/example/ejemplorv/MainActivity.kt
@@ -6,7 +6,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ejemplorv.databinding.ContactosBinding
-
+import com.google.firebase.analytics.FirebaseAnalytics
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,19 +19,19 @@ class MainActivity : AppCompatActivity() {
 
 
         contactos.vistaContactos.adapter=ContactosAdapter(listOf(
-        Contacto("Juan","648362245",  "hombre"),
-        Contacto("Jorge","648362245", "hombre"),
-        Contacto("Sheila","648362245","mujer"),
-        Contacto("Juana","648365465","mujer"),
-        Contacto("Carla","648362245","mujer"),
-        Contacto("Marta","644892245","mujer"),
-        Contacto("Paco","648362456","mujer"),
-        Contacto("Fabián","636962245","hombre"),
-        Contacto("Jonathan","123362245","hombre"/*"htttps"+(100+Math.random().toInt())*/),
-        Contacto("Erick","612362245","hombre"),
-        Contacto("Sandra","648362789","mujer"),
-        Contacto("Leo","6483625436","hombre"),
-        Contacto("Messi","648365642","hombre")
+        Contacto(1,"Juan","648362245",  "hombre"),
+        Contacto(2,"Jorge","648362245", "hombre"),
+        Contacto(3,"Sheila","648362245","mujer"),
+        Contacto(4,"Juana","648365465","mujer"),
+        Contacto(5,"Carla","648362245","mujer"),
+        Contacto(6,"Marta","644892245","mujer"),
+        Contacto(7,"Paco","648362456","mujer"),
+        Contacto(8,"Fabián","636962245","hombre"),
+        Contacto(9,"Jonathan","123362245","hombre"/*"htttps"+(100+Math.random().toInt())*/),
+        Contacto(10,"Erick","612362245","hombre"),
+        Contacto(11,"Sandra","648362789","mujer"),
+        Contacto(12,"Leo","6483625436","hombre"),
+        Contacto(13,"Messi","648365642","hombre")
         )
             ,
             object : ContactoPulsadoListener{
@@ -43,7 +43,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         )
+        val analytics :FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle= Bundle()
 
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen",bundle)
 
     }
 }
